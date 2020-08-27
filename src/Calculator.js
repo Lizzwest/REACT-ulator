6d07801a4427ec51be135933dba82c660bcf1e8b
Lizz <lizwesterband@gmail.com>
2020-08-26 19:06:12 -0700

    functional calculator; no styling

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,22 +6,41 @@ class Calculator extends Component {
         this.state = {
             num1: 0,
             num2: 0,
-            result: ""
+            result: "Result goes here"
         }
       }
+      setNum = (e, num) => {
+        this.setState({ [num]: e.target.value })
+    }
+    getResult = () => {
+        console.log(typeof this.state.num1, 'num 1')
+        let result = parseInt(this.state.num1) + parseInt(this.state.num2)
+        console.log('result ', result)
+        this.setState({
+            result: result
+        })
+    }  
 
     render() {
         
         return (
-            <div className="container">
-  <h1>Add with React!</h1>
-
+<div className="container">
+    <h1>Add with React!</h1>
   <div className="add">
-    <input type="text" />
+  <input type="number"
+    name="num1"
+    placeholder="Enter your first number"
+    value={this.state.num1}
+     onChange={e => this.setNum(e, 'num1')} />
     <span>+</span>
-    <input type="text" />
-    <button>=</button>
-    <h3>Addition results go here!</h3>
+    <input type="number"
+    name="num2"
+    placeholder="Enter your second number"
+    value={this.state.num2}
+    onChange={e => this.setNum(e, 'num2')} />
+    <button onClick={this.getResult}>=</button>
+     <h3>{this.state.result}</h3>
+
   </div>
 </div>
     )
